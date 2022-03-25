5f7185f872431ab2ece54dcb75f2a37a6c4b31df
Nalanihv <nalanihevi@gmail.com>
2022-03-25 11:09:37 -0600

    agrego artibutos y sus getters and setters

--- src/calculadora.java
+++ src/calculadora.java
@@ -1,6 +1,26 @@
+import java.util.ArrayList;
+
 public class calculadora {
 
     // ArrayList,R,S
-    //private String marca;
-   // private String
+    private String marca;
+   private boolean esCientifica;
+   private double primerValor;
+   private double segundoValor;
+
+    public String getMarca() {return marca;}
+
+    public void setMarca(String marca) {this.marca = marca;}
+
+    public boolean isEsCientifica() {return esCientifica;}
+
+    public void setEsCientifica(boolean esCientifica) {this.esCientifica = esCientifica;}
+
+    public double getPrimerValor() {return primerValor;}
+
+    public void setPrimerValor(double primerValor) {this.primerValor = primerValor;}
+
+    public double getSegundoValor() {return segundoValor;}
+
+    public void setSegundoValor(double segundoValor) {this.segundoValor = segundoValor;}
 }
